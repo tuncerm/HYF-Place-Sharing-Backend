7bcffb318459cac2380f7d69fef54cf3812fa046
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-05 13:54:18 +0100

    Added error handler

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(bodyParser.json());
 app.use('/api/users', usersRoutes);
 app.use('/api/places', placesRoutes);
 
-
+app.use((error, req, res, next)=>{
+    if(res.headerSend){
+        return next(error);
+    }
+    res.status(error.code || 500).json({message: error.message || "Something went wrong!"})
+});
 
 app.listen(5000);
